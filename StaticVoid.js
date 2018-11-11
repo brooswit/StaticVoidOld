e53341bd67eabc8bab3afa374407fd137d3e72e6
brooswit <brooswit@OldBoi.local>
2018-11-11 10:35:09 -0800

    AUTOMATED DEPLOYMENT

--- StaticVoid.js
+++ StaticVoid.js
@@ -44,6 +44,21 @@ module.exports = class StaticVoidHost {
             sessionElement.destroy();
         });
 
+        (async ()=>{
+            let ping = 0
+            while(true) {
+                let snapshot = await session.getSnapshot({ping}) || {ping}
+                let startTime = Date.now()
+                let rawSnap = JSON.stringify(snapshot)
+                await new Promise((done) => ws.send(rawSnap, done))
+                let endTime = Date.now()
+                ping = endTime - startTime
+                let throttle = this._syncThrottle
+                let delay = Math.min(Math.max(1, throttle - ping), throttle)
+                await new Promise((done) => setTimeout(done, delay))
+            }
+        })()
+
     }
 
     registerBehavior(Behavior) {
