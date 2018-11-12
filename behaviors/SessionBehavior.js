5dd08aa1eea1497c696ac5b421c8a0dd7050dd2b
brooswit <brooswit@OldBoi.local>
2018-11-11 19:27:34 -0800

    AUTOMATED DEPLOYMENT

--- behaviors/SessionBehavior.js
+++ behaviors/SessionBehavior.js
@@ -3,5 +3,6 @@ const Behavior = require('../classes/Behavior')
 module.exports = class SessionBehavior extends Behavior {
     constructor(parent, state) {
         super(parent, state);
+        const {ws} = state;
     }
 }
