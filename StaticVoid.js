4156d5169f218994a54e94bfbd6a04c359fdf344
brooswit <brooswit@OldBoi.local>
2018-11-11 19:01:50 -0800

    AUTOMATED DEPLOYMENT

--- StaticVoid.js
+++ StaticVoid.js
@@ -42,6 +42,7 @@ module.exports = class StaticVoidHost {
 
         ws.on('close', () => {
             sessionElement.destroy();
+            sessionElement = null;
         });
 
         (async ()=>{
