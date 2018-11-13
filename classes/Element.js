b9680c1858816de118f9296912cbcc634f69940c
brooswit <brooswit@OldBoi.local>
2018-11-12 18:56:36 -0800

    AUTOMATED DEPLOYMENT

--- classes/Element.js
+++ classes/Element.js
@@ -8,7 +8,7 @@ module.exports = class ElementFactory {
     }
 
     buildElement(state={}, controllers=[]) {
-        let newElement = new Element(this._Controllers, state, this._id++);
+        let newElement = new Element(this._Controllers, state);
         for(controllerIndex in controllers) {
             let controllerName = controllers[controllerIndex];
             newElement.addController(controllerName)
@@ -20,7 +20,7 @@ module.exports = class ElementFactory {
     }
 
     provisionId() {
-        
+        return this._id++;
     }
 }
 
