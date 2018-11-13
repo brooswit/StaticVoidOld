13dc7252b41d6eb4b7356108312e827124f5a334
brooswit <brooswit@OldBoi.local>
2018-11-12 19:24:20 -0800

    AUTOMATED DEPLOYMENT

--- StaticVoid.js
+++ StaticVoid.js
@@ -44,6 +44,7 @@ class StaticVoid extends ElementManager {
         sessionElement.on('destroy', () => {
             sessionElement = null;
         });
+
         asynchronously(async () => {
             let getSnapshotStartTime;
             let getSnapshotEndTime;
