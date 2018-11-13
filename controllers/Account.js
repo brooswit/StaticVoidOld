d9e831bbdea0f8ec20efe6d58f7a5cb373fc3bcb
brooswit <brooswit@OldBoi.local>
2018-11-12 19:42:15 -0800

    AUTOMATED DEPLOYMENT

--- controllers/Account.js
+++ controllers/Account.js
@@ -3,8 +3,8 @@ const Controller = require('../classes/Controller');
 module.exports = class Account extends Controller {
     constructor(parent, state) {
         super(parent, state);
-        this.parent.on('session_login', (o)=>{
-
+        this.parent.on('session_login', ({username, passwordHash})=>{
+            this.login(username, passwordHash)
         });
     }
 
