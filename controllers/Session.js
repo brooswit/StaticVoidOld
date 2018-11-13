87c0a8ca5b5feb01c5679cf8e92aed9f9a47cde7
brooswit <brooswit@OldBoi.local>
2018-11-12 19:34:03 -0800

    AUTOMATED DEPLOYMENT

--- controllers/Session.js
+++ controllers/Session.js
@@ -3,6 +3,8 @@ const Controller = require('../classes/Controller');
 module.exports = class Session extends Controller {
     constructor(parent, state) {
         super(parent, state);
-        this.parent.on('sessionMessage', this.)
+        this.parent.on('sessionMessage', this.injestMessage);
     }
+
+    in
 }
