d1f566fcff52b0d5b752eb7565e642045c90d75e
brooswit <brooswit@OldBoi.local>
2018-11-12 18:52:44 -0800

    AUTOMATED DEPLOYMENT

--- classes/Element.js
+++ classes/Element.js
@@ -8,7 +8,7 @@ module.exports = class ElementFactory {
     }
 
     buildElement(state={}, controllers=[]) {
-        let newElement = new Element(this._id++, state, this._Controllers);
+        let newElement = new Element(this._Controllers, this._id++, state);
         for(controllerIndex in controllers) {
             let controllerName = controllers[controllerIndex];
             newElement.addController(controllerName)
