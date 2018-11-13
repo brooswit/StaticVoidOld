3cae894e4e129b4323eae561bdb5966966f8b368
brooswit <brooswit@OldBoi.local>
2018-11-12 19:27:46 -0800

    AUTOMATED DEPLOYMENT

--- classes/Controller.js
+++ classes/Controller.js
@@ -1,7 +1,7 @@
 module.exports = class Controller {
     constructor(parent, state = {}) {
-        this._parent = parent;
-        this,_parent.on('destroy', this.destroy);
+        this.parent = parent;
+        this,parent.on('destroy', this.destroy);
 
         this._state = state;
     }
