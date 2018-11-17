974f6dec37d8629036d3a61405ab024067be980f
brooswit <brooswit@OldBoi.local>
2018-11-16 17:58:13 -0800

    AUTOMATED DEPLOYMENT

--- classes/Controller.js
+++ classes/Controller.js
@@ -2,9 +2,9 @@ const EventEmitter = require('events');
 
 module.exports = class Controller extends EventEmitter {
     constructor(parent, state = {}) {
-        this.parent = parent;
-        this,parent.on('destroy', this.destroy);
-        this,parent.on('snapshot', this.decorateSnapshot);
+        this._element = element;
+        this,_element.on('destroy', this.destroy);
+        this,element.on('snapshot', this.decorateSnapshot);
 
         this._state = state;
     }
