4d2acb4dc7b3ff787547a306be855c74384bd4b4
brooswit <brooswit@OldBoi.local>
2018-11-18 11:31:45 -0800

    AUTOMATED DEPLOYMENT

--- classes/EventManager.js
+++ classes/EventManager.js
@@ -14,7 +14,7 @@ class EventyHandler extends Promise {
         internalEvents.on('dettached', this._onDettached);
     }
     _onAttached(source) {
-        source.on(this._eve);
+        source.on(this._eventName, this._callback, this._payload);
 
     }
     _onDetached(source) {
