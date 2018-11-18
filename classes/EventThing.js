cbd6a57e490393c42b619a570562a5743586ecc5
brooswit <brooswit@OldBoi.local>
2018-11-17 20:42:15 -0800

    AUTOMATED DEPLOYMENT

--- classes/EventThing.js
+++ classes/EventThing.js
@@ -20,7 +20,7 @@ class EventHandler {
 
     trigger() {
         if (this._off) return;
-        if (++this._triggerCount > this._triggerLimit) this.off();
+        if ( > this._triggerLimit) this.off();
         return this._cb.apply(null, arguments);
     }
 
