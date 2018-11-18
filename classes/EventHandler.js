33d76ea00a437f03eb6648bd1e692a28481ae248
brooswit <brooswit@OldBoi.local>
2018-11-18 08:56:48 -0800

    AUTOMATED DEPLOYMENT

---
+++ classes/EventHandler.js
@@ -0,0 +1,43 @@
+const EventHandler = require('./EventHandler')
+const EventEmitter = require('events');
+
+class EventManager {
+    constructor() {
+        this._isClosed = false;
+
+        this._emitter = new EventEmitter();
+        this._internalEmitter = new EventEmitter();
+    }
+
+    trigger(eventName) {
+        if(this._isClosed) return;
+        return this._emitter.emit(eventName);
+    }
+
+    on(eventName, callback) {
+        if(this._isClosed) return;
+        let eventHandler = new EventHandler(this._emitter, this._internalEmitter, eventName, callback, false, Array.prototype.slice.call(arguments, 2));
+        return eventHandler;
+    }
+
+    once(eventName, callback) {
+        if(this._isClosed) return;
+        let eventHandler = new EventHandler(this._emitter, this._internalEmitter, eventName, callback, 1, Array.prototype.slice.call(arguments, 2));
+        return eventHandler;
+    }
+
+    off(eventName) {
+        if(this._isClosed) return;
+        this._internalEmitter.emit(`off:${eventName}`);
+    }
+
+    close() {
+        if(this._isClosed) return;
+        this._isClosed = true;
+        this._internalEmitter.emit('close');
+    }
+}
+
+EventManager.Interface = EventManagerInterface;
+
+module.exports = EventManager;
