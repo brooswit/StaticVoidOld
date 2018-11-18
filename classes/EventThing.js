972d34af8fb26e9a124f13ed22ff6afd1b148c22
brooswit <brooswit@OldBoi.local>
2018-11-17 20:22:00 -0800

    AUTOMATED DEPLOYMENT

--- classes/EventThing.js
+++ classes/EventThing.js
@@ -10,7 +10,7 @@ class EventHandler {
 class EventEmitter {
     constructor() {
         this._nextEventHandlerIndex = 0;
-        this._callbackLookup = {};
+        this._eventHandlerLookup = {};
         this._eventLookup = {}
     }
 
