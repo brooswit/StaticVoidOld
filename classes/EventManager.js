200c5bb42d51681ae696c85ed24f9cbb4e357519
brooswit <brooswit@OldBoi.local>
2018-11-18 10:26:08 -0800

    AUTOMATED DEPLOYMENT

--- classes/EventManager.js
+++ classes/EventManager.js
@@ -26,7 +26,7 @@ class EventyView {
     }
 
     on(eventName, callback, payload) {
-        return new EventHandler(this._source, this._internalEvents, eventName, callback, payload);
+        return new EventyHandler(this._source, this._internalEvents, eventName, callback, payload);
     }
 
     trigger() { return this._source && this._source[arguments.callee.name].apply(this._source, arguments); }
