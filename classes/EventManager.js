66a2a69ffe77bb51681ba9c6b47a2f1cad7afa56
brooswit <brooswit@OldBoi.local>
2018-11-18 11:59:55 -0800

    AUTOMATED DEPLOYMENT

--- classes/EventManager.js
+++ classes/EventManager.js
@@ -64,6 +64,7 @@ class EventyView {
     close() {
         this._internalEvents.emit('closed');
     }
+
     trigger() { return this._source && this._source[arguments.callee.name].apply(this._source, arguments); }
 
 }
