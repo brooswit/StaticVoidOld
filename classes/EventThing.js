7ed3d50bfd0872b4924eed64f9a0ab815b71a9c3
brooswit <brooswit@OldBoi.local>
2018-11-17 20:34:17 -0800

    AUTOMATED DEPLOYMENT

---
+++ classes/EventThing.js
@@ -0,0 +1,61 @@
+class EventHandler {
+    constructor(eventManager, eventName, callback, once = false) {
+        this._manager = eventManager;
+        this._once = once;
+        this._off = false;
+        this._manager._eventEmitter[once ? 'once' : 'on']('eventName', callback);
+    }
+
+    trigger() {
+        if (this._off) return;
+        if (this.once) this.off();
+        return callback.apply(null, arguments);
+    }
+
+    off() {
+        if (this._off) return;
+        this._off = true;
+        this._manager._eventEmitter.off('eventName', callback);
+    }
+}
+
+class EventManager {
+    constructor() {
+        this._eventEmitter = new EventEmitter();
+        this._internalEventEmitter = new EventEmitter();
+    }
+
+    on(eventName, callback) {
+        return new EventHandler(this, eventName, callback);
+    }
+
+    close() {
+        this.
+    }
+}
+class EventInterface {
+    constructor(parent) {
+        this._parent = parent;
+    }
+
+
+    close() {
+
+    }
+}
+class EventInterfaceFactory {
+    constructor() {
+
+    }
+
+    newInterface() {
+        return new EventInterface(this);
+    }
+}
+class EventManager extends EventInterfaceFactory {
+    constructor() {
+
+    }
+
+
+}
