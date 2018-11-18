b89fa1d845fee8579323dfdea2b458dc97dbc565
brooswit <brooswit@OldBoi.local>
2018-11-18 11:25:44 -0800

    AUTOMATED DEPLOYMENT

--- classes/EventManager.js
+++ classes/EventManager.js
@@ -10,6 +10,7 @@ class EventyHandler extends Promise {
         this._eventName = eventName;
         this._callback = callback;
         this._payload = payload;
+        this.source
     }
     off() {
 
