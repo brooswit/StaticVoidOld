771ccac1f1979e40a8a665816b10ac3d339946d0
brooswit <brooswit@OldBoi.local>
2018-11-18 02:12:13 -0800

    AUTOMATED DEPLOYMENT

--- classes/Controller.js
+++ classes/Controller.js
@@ -9,9 +9,9 @@ module.exports = class Controller extends EventManager.Interface {
         this._element = element;
         this._managerEventInterface = new EventManager.Interface(this._manager);
         
-        this._elementEventInterface = new EventManager.Interface(this._element);
-        this,_elementEventInterface.on('destroy', this.remove);
-        this,_elementEventInterface.on('snapshot', this.snapshot);
+        this. = new EventManager.Interface(this._element);
+        this.on('destroy', this.remove);
+        this.on('snapshot', this.snapshot);
 
         this._state = state;
     }
