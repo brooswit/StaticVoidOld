94e593849aed013e25070305a1d0a2061e7eff23
brooswit <brooswit@OldBoi.local>
2018-11-18 03:02:45 -0800

    AUTOMATED DEPLOYMENT

--- classes/Element.js
+++ classes/Element.js
@@ -9,6 +9,7 @@ module.exports = class Element extends EventManager {
     attach(world) {
         if (this._isDestroyed) return;
         if (this.world === world) return;
+        if (this.world)
         this.world = new EventInterface(world);
         this.world.on('snapshot', this.snapshot);
         this.trigger('attached');
