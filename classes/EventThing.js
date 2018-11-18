61a7e700ed3e14151d8e143b25315a2e2eca0db3
brooswit <brooswit@OldBoi.local>
2018-11-17 20:36:40 -0800

    AUTOMATED DEPLOYMENT

--- classes/EventThing.js
+++ classes/EventThing.js
@@ -5,9 +5,9 @@ class EventHandler {
         this._off = false;
 
         this._manager._eventEmitter[once ? 'once' : 'on']('eventName', callback);
-        this._manager._internalEventEmitter.once('close', off);
-        this._manager._internalEventEmitter.once('off', off);
-        this._manager._internalEventEmitter.once(`off', off);
+        this._manager._internalEventEmitter.once(`close`, off);
+        this._manager._internalEventEmitter.once(`off`, off);
+        this._manager._internalEventEmitter.once(`off:${}`, off);
 
     trigger() {
         if (this._off) return;
