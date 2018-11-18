800d0cde2d1bbf2b750f42acce9755ffbd07c476
brooswit <brooswit@OldBoi.local>
2018-11-18 03:35:01 -0800

    AUTOMATED DEPLOYMENT

--- classes/Element.js
+++ classes/Element.js
@@ -11,7 +11,7 @@ module.exports = class Element extends EventManager {
     }
 
     root() {
-        this.parent ? this.
+        this.parent ? this.parent.root() : this
     }
 
     attach(parent) {
