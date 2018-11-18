c52e7b6a09961cf4f7d4bd53d959a081986de75c
brooswit <brooswit@OldBoi.local>
2018-11-17 20:55:31 -0800

    AUTOMATED DEPLOYMENT

--- classes/EventThing.js
+++ classes/EventThing.js
@@ -1,3 +1,4 @@
+function
 class EventHandler {
     constructor(eventManager, eventName, callback, triggerLimit) {
         this._manager = eventManager;
@@ -10,7 +11,7 @@ class EventHandler {
         assert(this._manager instanceof EventManager);
         assert(typeof this._name === 'string');
         assert(typeof this._cb === 'function');
-        assert(that(this._triggerLimit).is.a(Number).or.a(Boolean)) === 'number' || typeof this._triggerLimit === 'boolean');
+        assert(that(this._triggerLimit).is.a(Number).or.a(Boolean)));
         assert(typeof this._triggerLimit === 'number' || typeof this._triggerLimit === 'boolean');
 
         this._off = false;
