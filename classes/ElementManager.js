ff117efac2e48e7999200c77202c65d39faae5b7
brooswit <brooswit@OldBoi.local>
2018-11-18 00:08:04 -0800

    AUTOMATED DEPLOYMENT

---
+++ classes/ElementManager.js
@@ -0,0 +1,31 @@
+const EventManager = require('./EventManager');
+const Controller = require('./Controller');
+const Element = require('./Element');
+
+module.exports = class ElementManager extends EventManager {
+    constructor (Controllers = [], startId = 0) {
+        this._Controllers = Controllers;
+        this._id = startId;
+    }
+
+    buildElement(controllerNames = [], state = {}) {
+        let newElement = new Element(this, state);
+        for(controllerIndex in controllerNames) {
+            let controllerName = controllerNames[controllerIndex];
+            newElement.addController(controllerName);
+        }
+        this.trigger('newElement', newElement);
+        return newElement;
+    }
+
+    registerController(Controller) {
+        this._Controllers[Controller.name] = Controller;
+    }
+
+    attachController(element, controllerName, state) {
+        let newController = new (this._Controllers[controllerName] || Controller)(this, element, this._id++, state);
+        this.trigger('newController', newController);
+        this.trigger(`new${controllerName}`, newController);
+        return newController;
+    }
+}
