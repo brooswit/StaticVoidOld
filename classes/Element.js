5ad8b90478d50ed00eaf7268f3544faf1677aa6f
brooswit <brooswit@OldBoi.local>
2018-11-18 04:08:08 -0800

    AUTOMATED DEPLOYMENT

--- classes/Element.js
+++ classes/Element.js
@@ -5,7 +5,7 @@ module.exports = class Element extends EventManager {
         super();
         this.state = {};
         this._parent = null;
-        this._parentInterface = new EventManagerInterface(parent)
+        this.parent = new EventManagerInterface(parent)
         this._isDestroyed = false;
 
         if (parent) this.attach(parent);
@@ -29,7 +29,7 @@ module.exports = class Element extends EventManager {
         this.detach();
 
         this._parent = parent;
-        this._parentInterface.attach(this._parent);
+        this.parent.attach(this._parent);
 
         this.trigger('attached');
     }
@@ -39,7 +39,7 @@ module.exports = class Element extends EventManager {
         if (!this._parent) return;
 
         this._parent = null;
-        this._parentInterface.detach();
+        this.parent.detach();
 
         this.trigger('dettached');
     }
