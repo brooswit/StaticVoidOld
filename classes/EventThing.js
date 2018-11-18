6342afceaa6ca8d21caeee00462873fe0b434a62
brooswit <brooswit@OldBoi.local>
2018-11-17 22:54:33 -0800

    AUTOMATED DEPLOYMENT

--- classes/EventThing.js
+++ classes/EventThing.js
@@ -20,24 +20,18 @@ class EventHandler extends Promise {
 
         this._eventInterface = new EventInterface();
         this._eventInterface.on(
-            this._emitter,
-            'trigger',
-            this._handleEvent
+            this._emitter, 'trigger', this._handleEvent
         );
         this._eventInterface.once(
             this._emitter, `off`, this._eventInterface.close
         );
 
         this._eventInterface.on(
-            this._manager._emitter,
-            eventName,
-            this._handleEvent
+            this._manager._emitter, eventName, this._handleEvent
         );
         
         this._eventInterface.once(
-            this._manager._internalEmitter,
-            eventName,
-            this._handleEvent
+            this._manager._internalEmitter, eventName, this._handleEvent
         );
         this._eventInterface.once(
             this._manager._internalEmitter,
