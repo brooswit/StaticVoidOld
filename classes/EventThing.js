c32e2ee5afbd1225063f097b4459b146dc687284
brooswit <brooswit@OldBoi.local>
2018-11-17 22:29:43 -0800

    AUTOMATED DEPLOYMENT

--- classes/EventThing.js
+++ classes/EventThing.js
@@ -37,7 +37,7 @@ class EventHandler extends Promise {
 
     on(cb) {
         if (this._off) return;
-        this._manager.on()
+        this._manager.on(this._name, cb);
         this._emitter.on('trigger', cb);
         return this;
     }
