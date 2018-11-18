a3759b6115eb0c46b5d9b7e33617d197bc478d61
brooswit <brooswit@OldBoi.local>
2018-11-17 22:17:01 -0800

    AUTOMATED DEPLOYMENT

--- classes/EventThing.js
+++ classes/EventThing.js
@@ -72,7 +72,7 @@ class EventManager {
     }
 
     get(eventName) {
-        this.emit(`getEventHandler:${eventName}`,)
+        this.emit(`getEventHandler:${eventName}`, [])
         if(this._isClosed) return;
         return this._events[eventName] = this._events[eventName] || new EventHandler(this, eventName);
     }
