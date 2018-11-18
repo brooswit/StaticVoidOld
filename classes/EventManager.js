fd8bfb365de9951578bce7bb0d3936a401c9440c
brooswit <brooswit@OldBoi.local>
2018-11-17 23:39:31 -0800

    AUTOMATED DEPLOYMENT

--- classes/EventManager.js
+++ classes/EventManager.js
@@ -27,16 +27,13 @@ class EventHandler extends Promise {
 
         this._name = eventName;
         this._cb = callback;
-        
-
+        this._context = c
         this._managerEventInterface = new EventManagerInterface(emitter);
         this._managerInternalEventInterface = new EventManagerInterface(internalEmitter);
         this._triggerLimit = triggerLimit === true ?
             1 : typeof triggerLimit === 'number' ?
             triggerLimit : false;
 
-        assert(emitter instanceof EventEmitter);
-        assert(internalEmitter instanceof EventEmitter);
         assert(typeof this._name === 'string');
         assert(typeof this._cb === 'function');
         assert(typeof this._triggerLimit === 'number' || typeof this._triggerLimit === 'boolean');
