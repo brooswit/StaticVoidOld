e5c1c5ddc1c4076b9163b886318c3c82b7f64ea9
brooswit <brooswit@OldBoi.local>
2018-11-18 09:32:18 -0800

    AUTOMATED DEPLOYMENT

--- classes/EventManager.js
+++ classes/EventManager.js
@@ -19,7 +19,7 @@ class EventyView {
         this._source = eventy;
         this._emitter.emit('source_changed');
     }
-    trigger() { return this._source.trigger.apply(this._source, arguments); }
+    trigger() { return this._source[arguments.callee.name].apply(this._source, arguments); }
     trigger() { return this._source.trigger.apply(this._source, arguments); }
 
 }
