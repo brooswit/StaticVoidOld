6eded61b868ce18e5ebbef7bd90cd4eb8a68207b
brooswit <brooswit@OldBoi.local>
2018-11-18 02:57:12 -0800

    AUTOMATED DEPLOYMENT

--- classes/Element.js
+++ classes/Element.js
@@ -10,6 +10,7 @@ module.exports = class Element extends EventManager {
     attach(world) {
         if (this._world) return;
         this._world = world;
+        this.trigger('attached', world)
     }
 
     addController(controllerName, state={}) {
