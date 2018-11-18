65ff12cddc83bc042a110c038639d27d5ce4b963
brooswit <brooswit@OldBoi.local>
2018-11-18 02:48:56 -0800

    AUTOMATED DEPLOYMENT

--- classes/Controller.js
+++ classes/Controller.js
@@ -1,6 +1,6 @@
 const EventManager = require('./EventManager');
 
-module.exports = class ElementControllerInterface extends EventManager.Interface {
+module.exports = class Controller extends EventManager.Interface {
     constructor(element) {
         super(element);
         this._element = element;
