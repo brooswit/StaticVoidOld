1d3e4d9d2296413d7bfd6a827bd130a2dc1fabee
brooswit <brooswit@OldBoi.local>
2018-11-18 19:10:18 -0800

    AUTOMATED DEPLOYMENT

--- classes/EventManager.js
+++ classes/EventManager.js
@@ -4,7 +4,7 @@ class EventHandler {
 class View {
     constructor(abra) {
         this._source = abra;
-        this._events = 
+        this._events = this._source._events;
     }
 }
 class Abra {
