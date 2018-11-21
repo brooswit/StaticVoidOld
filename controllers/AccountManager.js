4fa9960bbef86123266f5eb80e02518dcf755053
brooswit <brooswit@OldBoi.local>
2018-11-21 14:03:40 -0800

    AUTOMATED DEPLOYMENT

---
+++ controllers/AccountManager.js
@@ -0,0 +1,13 @@
+const Element = require('../classes/Element');
+
+module.exports = class Session extends Element {
+    constructor(parent, state) {
+        super(parent && parent.root(), state);
+
+        this.on('rpc', this._triggerRPC);
+    }
+
+    _triggerRPC(payload) {
+        this.trigger(`rpc_${payload.rpc.command}`, payload.options);
+    }
+}
