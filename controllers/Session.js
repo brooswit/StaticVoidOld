24f273240ce641bc1ecf811bde89a931102ea1b1
brooswit <brooswit@OldBoi.local>
2018-11-20 18:31:21 -0800

    AUTOMATED DEPLOYMENT

--- controllers/Session.js
+++ controllers/Session.js
@@ -1,12 +1,12 @@
 const Element = require('../classes/Element');
 
 module.exports = class Session extends Element {
-    constructor(element, state) {
-        super(element && element.root, state);
-        this.element.on('rpc', this.injestMessage);
+    constructor(parent, state) {
+        super(parent && parent.root(), state);
+        this.parent.on('rpc', this.injestMessage);
     }
 
     injestMessage(payload) {
-        this.element.trigger(`rpc_${payload.rpc.command}`, payload.options);
+        this.parent.trigger(`rpc_${payload.rpc.command}`, payload.options);
     }
 }
