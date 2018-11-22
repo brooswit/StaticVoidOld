818f3b3907c3943d6bf44ae509a73ce2a94f591e
brooswit <brooswit@OldBoi.local>
2018-11-22 01:12:23 -0800

    AUTOMATED DEPLOYMENT

--- classes/Abra.js
+++ classes/Abra.js
@@ -36,7 +36,7 @@ class EventQuery {
             function handleResponse(index, error, result) {
                 errored = errored || !!error;
                 results[index] = result;
-                errors[index]
+                errors[index] = errors
                 for(resultIndex in results) {
                     if(!results[resultIndex]) return;
                 }
