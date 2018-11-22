b74d47210182a48ed2e587192f4748c6071d2dbb
brooswit <brooswit@OldBoi.local>
2018-11-22 00:05:27 -0800

    AUTOMATED DEPLOYMENT

--- classes/Abra.js
+++ classes/Abra.js
@@ -21,7 +21,7 @@ class EventQuery {
                         result: null,
                     });
                 },
-                ()=>{
+                (index)=>{
                     
                 }
             );
