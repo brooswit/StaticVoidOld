ba391e3893f94ef87a8a6d48bb5e51e7836dcb74
brooswit <brooswit@OldBoi.local>
2018-11-23 12:34:57 -0800

    AUTOMATED DEPLOYMENT

--- classes/Abra.js
+++ classes/Abra.js
@@ -216,9 +216,8 @@ class ElementState extends ElementInterface {
 
         this._queryEmitter = new QueryEmitter();
         this._callbackRegistry = new CallbackRegistry();
-
         this._data = {};
-        this.source = this;
+
 
         this,_isDestroyed = false;
 
