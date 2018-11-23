994eb7773cc6cbb06c8aaf10ea6d365cc9e8eae7
brooswit <brooswit@OldBoi.local>
2018-11-23 14:56:06 -0800

    AUTOMATED DEPLOYMENT

--- classes/Abra.js
+++ classes/Abra.js
@@ -98,6 +98,7 @@ class ElementQueryHook {
     }
 
     _onClosed() {
+        
         this._source._internalEvents.off('source_changed', this._onSourceChanged);
         this._source._internalEvents.off('closed', this._onClosed);
     }
