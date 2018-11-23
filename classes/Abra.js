c77da28e4a647b9ff1cecd4bea0abd6fa546307c
brooswit <brooswit@OldBoi.local>
2018-11-23 08:52:21 -0800

    AUTOMATED DEPLOYMENT

--- classes/Abra.js
+++ classes/Abra.js
@@ -147,7 +147,7 @@ class View {
     }
 
     wrap(methodName) {
-        let wrappedMethod = function () {
+        this._wrappedMethod = function () {
             if (!this.exists()) return;
             this._source[methodName].call(this._source, arguments);
         }
