eeb486ba5faf363f4b762dffa818da91d3e002e9
brooswit <brooswit@OldBoi.local>
2018-11-23 01:29:12 -0800

    AUTOMATED DEPLOYMENT

--- classes/Abra.js
+++ classes/Abra.js
@@ -151,7 +151,7 @@ class ElementView extends ElementInterface {
     }
 
     hook(eventName, promise) {
-        let eventHook = new EventHook()
+        let eventHook = new EventHook(this)
         let eventHandler = null;
         if (this._sourceElement) {
             eventHandler = rehook(null, this._sourceElement)
