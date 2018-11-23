7185c4350f64aa73d845ed8215511f2914919bfe
brooswit <brooswit@OldBoi.local>
2018-11-22 22:37:11 -0800

    AUTOMATED DEPLOYMENT

--- classes/Abra.js
+++ classes/Abra.js
@@ -132,7 +132,7 @@ class Abra {
         this._callbackRegistry = new EventEmitter();
 
         this.parentView = new ElementView();
-        
+        this.parent
         this.attach(parent);
     }
     attach(newParent) {
