e4ad38bb64481e8ab705235103f425325d81d8ec
brooswit <brooswit@OldBoi.local>
2018-11-23 08:44:50 -0800

    AUTOMATED DEPLOYMENT

--- classes/Abra.js
+++ classes/Abra.js
@@ -152,7 +152,7 @@ class View {
         this[methodName] = this[methodName] || wrap;
         function wrap() {
             if (!this.exists()) return;
-            
+            this._source[]
         }
     }
 
