4ea8a97316d6c9d3c53573a1c1edda27776c961c
brooswit <brooswit@OldBoi.local>
2018-11-23 19:24:06 -0800

    AUTOMATED DEPLOYMENT

--- controllers/AccountManager.js
+++ controllers/AccountManager.js
@@ -1,29 +1,8 @@
 
 class AccountManager extends Element {
     constructor(parent, options) {
-        super(parent)
-        this._port = process.env.PORT || options.port || 8080;
-        this._syncThrottle = options.syncThrottle || 1000/3;
-
-        this._app = express();
-
-        enableWs(_app);
-
-        _app.use(express.static(path.join(__dirname, 'public')))
-            .set('views', path.join(__dirname, 'views'))
-            .set('view engine', 'ejs')
-            .get('/', this._handleWebRequest)
-            .ws('/stream', this._handleStreamRequest)
-            .listen(this._port);
-    }
-
-    _handleWebRequest(req, res) {
-        new WebR
-        this.trigger('webRequest', {req, res});
-    }
-
-    _handleStreamRequest(ws) {
-        this.trigger('streamRequest', ws);
+        super(parent);
+        AccountManager
     }
 }
 
