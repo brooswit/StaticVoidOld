140a831bd1eb00600694c53a7232e2d6b569fbb8
brooswit <brooswit@OldBoi.local>
2018-11-24 14:43:55 -0800

    AUTOMATED DEPLOYMENT

---
+++ elements/AccountManager.js
@@ -0,0 +1,14 @@
+const Element = require('./Element');
+
+module.exports = class AccountManager extends Element {
+    constructor(parent, state) {
+        super(parent, state);
+        this.parent.on('rpc_login', ({username, passwordHash})=>{
+            this.login(username, passwordHash)
+        });
+    }
+
+    login(username, passwordHash) {
+        
+    }
+}
