9225f3de49848dd76dc95eebe86a17738833dc8b
brooswit <brooswit@OldBoi.local>
2018-11-24 14:42:26 -0800

    AUTOMATED DEPLOYMENT

--- elements/Client.js
+++ elements/Client.js
@@ -1,14 +1,7 @@
 const Element = require('./Element');
 
-module.exports = class Account extends Element {
-    constructor(parent, state) {
-        super(parent, state);
-        this.parent.on('rpc_login', ({username, passwordHash})=>{
-            this.login(username, passwordHash)
-        });
-    }
-
-    login(username, passwordHash) {
-        
+module.exports = class Client extends Element {
+    constructor(parent, options) {
+        super(parent, options);
     }
 }
