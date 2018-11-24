7f539c519c48803c301d445d6ba8139de92f29e6
brooswit <brooswit@OldBoi.local>
2018-11-23 19:25:28 -0800

    AUTOMATED DEPLOYMENT

--- controllers/AccountManager.js
+++ controllers/AccountManager.js
@@ -2,8 +2,10 @@
 class AccountManager extends Element {
     constructor(parent, options) {
         super(parent);
-        this.parent.hook('client_created', _onClientCreated)
+        this.parent.hook('client_created', _onClientCreated);
     }
+
+    _onClientCreated(newClient)
 }
 
 module.exports = ExpressManager
