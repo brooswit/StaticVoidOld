ade78d82494f9643f6847e0429f6eae55d86561d
brooswit <brooswit@OldBoi.local>
2018-11-23 16:25:39 -0800

    AUTOMATED DEPLOYMENT

--- elements/ExpressManager.js
+++ elements/ExpressManager.js
@@ -1,6 +1,6 @@
 
 class ExpressManager extends Element {
-    
+    constructor() {
         this._port = process.env.PORT || options.port || 8080;
         this._syncThrottle = options.syncThrottle || 1000/3;
 
