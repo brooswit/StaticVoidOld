09078c4898fa3321431a26ff547889b790e230e4
brooswit <brooswit@OldBoi.local>
2018-11-25 13:25:33 -0800

    AUTOMATED DEPLOYMENT

--- elements/Client.js
+++ elements/Client.js
@@ -3,5 +3,6 @@ const Element = require('./Element');
 module.exports = class Client extends Element {
     constructor(parent, options) {
         super(parent, options);
+        this.root().trigger('session_created', this);
     }
 }
--- elements/Session.js
+++ elements/Session.js
@@ -3,6 +3,6 @@ const Element = require('./Element');
 module.exports = class Session extends Element {
     constructor(parent, options) {
         super(parent, options);
-        this.root().trigger('sess')
+        this.root().trigger('session_created', this);
     }
 }
