5d04d58855ee90884e0e90054a4534649c68e132
brooswit <brooswit@OldBoi.local>
2018-11-25 13:29:02 -0800

    AUTOMATED DEPLOYMENT

--- elements/MemoryStore.js
+++ elements/MemoryStore.js
@@ -3,7 +3,7 @@ const Element = require('./Element');
 module.exports = class MemoryStore extends Element {
     constructor(parent, state) {
         super(parent, state);
-        this.parent.on('rpc_login', ({username, passwordHash})=>{
+        this.parent.on('save', ({path, value})=>{
             this.login(username, passwordHash)
         });
     }
