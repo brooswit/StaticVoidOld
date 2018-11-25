cf55476a0784d8b723d7b8b37b9f512bcea9c29a
brooswit <brooswit@OldBoi.local>
2018-11-25 13:16:09 -0800

    AUTOMATED DEPLOYMENT

--- elements/SessionManager.js
+++ elements/SessionManager.js
@@ -1,6 +1,6 @@
 const Element = require('./Element');
 
-module.exports = class SessionManager extends Element.View {
+module.exports = class SessionManager extends Element {
     constructor(parent, state) {
         super(parent, state);
         this.parent.on('client_created', this._onClientCreated);
