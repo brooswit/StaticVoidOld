527c2dd11539622d2aafbe9c8c2dd3bf0c071362
brooswit <brooswit@OldBoi.local>
2018-11-25 15:03:12 -0800

    AUTOMATED DEPLOYMENT

--- elements/ClientSession.js
+++ elements/ClientSession.js
@@ -1,6 +1,6 @@
 const Element = require('./Element');
 
-module.exports = class Session extends Element {
+module.exports = class ClientSession extends Element {
     constructor(parent, options) {
         super(parent, options);
         this.root().trigger('session_created', this);
