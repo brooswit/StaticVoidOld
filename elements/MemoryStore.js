75ab39045c766f43996de18004f7d602918ca0ea
brooswit <brooswit@OldBoi.local>
2018-11-25 13:31:32 -0800

    AUTOMATED DEPLOYMENT

--- elements/MemoryStore.js
+++ elements/MemoryStore.js
@@ -4,7 +4,7 @@ module.exports = class MemoryStore extends Element {
     constructor(parent, state) {
         super(parent, state);
         this.parent.on('save', ({path, value})=>{
-            let paths
+            let pth
             this.data
             this.login(username, passwordHash)
         });
