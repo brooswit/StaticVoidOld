4784ea57ab63a9010e40ab6edb05a643205283c2
brooswit <brooswit@OldBoi.local>
2018-11-25 13:40:25 -0800

    AUTOMATED DEPLOYMENT

--- elements/MemoryStore.js
+++ elements/MemoryStore.js
@@ -10,7 +10,7 @@ module.exports = class MemoryStore extends Element {
                 pointer = pointer[pathElementIndex] === undefined ? {} : pointer[pathElementIndex];
                 pathElement = pathElements[pathElementIndex];
             }
-            pointer[]
+            pointer[pathElements.length - 1] =
             this.data
             this.login(username, passwordHash)
         });
