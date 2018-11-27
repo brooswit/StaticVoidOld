328d99a6fdfe85fc32b8d670e64b52aad3dee98a
brooswit <brooswit@OldBoi.local>
2018-11-26 16:30:56 -0800

    AUTOMATED DEPLOYMENT

--- elements/AccountManager.js
+++ elements/AccountManager.js
@@ -3,10 +3,10 @@ const Element = require('./Element');
 module.exports = class AccountManager extends Element {
     constructor(parent, state) {
         super(parent, state);
-        this.parent.hook('session_created', this._onSessionCreated);
+        this.parent.hook('session_login', this._onSessionCreated);
     }
 
     _onSessionCreated(newSession) {
-        new Account(newSession);
+        new Account(this);
     }
 }
