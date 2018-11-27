2c241cf518e102ac2b1a07e116a7660449c666af
brooswit <brooswit@OldBoi.local>
2018-11-27 15:36:16 -0800

    AUTOMATED DEPLOYMENT

--- elements/Element.js
+++ elements/Element.js
@@ -198,7 +198,7 @@ class Element {
 
         this.parentView().hook('destroyed', this.destroy);
         this.parentView().hook('get_children', this._getThis);
-        this.parentView().hook(`get_children_${this.name}`);
+        this.parentView().hook(`get_${this.name}`);
         
         this.rootView().hook(`get_all_${this.name}`);
     }
