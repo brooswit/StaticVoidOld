a6b83192dabd4ab0b48baa3fa16b6919432c7247
brooswit <brooswit@OldBoi.local>
2018-11-29 23:05:47 -0800

    AUTOMATED DEPLOYMENT

--- elements/BaseStore.js
+++ elements/BaseStore.js
@@ -3,27 +3,11 @@ const Element = require('./Element')
 module.exports = class MemoryStore extends Element {
   constructor (parent, state) {
     super(parent, state)
-    this.rootView().hook('save', this._onSave);
-    this.rootView().hook('load', this._onLoad);
-    this      let pathElements = path.split('/')
-      let pointer = this.data
-      for (let pathElementIndex = 0; pathElementIndex < pathElements.length - 1; pathElementIndex++) {
-        pointer = pointer[pathElementIndex] === undefined ? {} : pointer[pathElementIndex]
-        pathElement = pathElements[pathElementIndex]
-      }
-      pointer[pathElements.length - 1] = value
-    })
-    this.parent.hook('load', ({ path, value }) => {
-      let pathElements = path.split('/')
-      let pointer = this.data
-      for (let pathElementIndex = 0; pathElementIndex < pathElements.length - 1; pathElementIndex++) {
-        pointer = pointer[pathElementIndex] === undefined ? {} : pointer[pathElementIndex]
-        pathElement = pathElements[pathElementIndex]
-      }
-      return pointer[pathElements.length - 1]
-    })
+    this.rootView().hook('save', this._onSave)
+    this.rootView().hook('load', this._onLoad)
   }
-  _onSave({collection, key, value}) {
-      
+  _onSave ({ collection, key, value }) {
+    this.save(collection, key, value)
   }
+  _
 }
