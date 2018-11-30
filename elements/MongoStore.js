a067664c7f90fe8e7ace101ee8d27dde22e9906b
brooswit <brooswit@OldBoi.local>
2018-11-30 00:52:47 -0800

    AUTOMATED DEPLOYMENT

--- elements/MongoStore.js
+++ elements/MongoStore.js
@@ -25,3 +25,42 @@ module.exports = class MongoStore extends BaseStore {
     return true
   }
 }
+
+var assert = require('assert');
+ 
+var cloud = true;
+ 
+var mongodbHost = '127.0.0.1';
+var mongodbPort = '27017';
+ 
+var authenticate ='';
+//cloud
+if (cloud) {
+ mongodbHost = 'YOURHOST.mlab.com';
+ mongodbPort = 'YOURPORT';
+ authenticate = 'YOURUSER:YOURPASSWORD@'
+}
+ 
+var mongodbDatabase = 'world';
+ 
+// connect string for mongodb server running locally, connecting to a database called test
+var url = 'mongodb://'+authenticate+mongodbHost+':'+mongodbPort + '/' + mongodbDatabase;
+ 
+ 
+// find and CRUD: http://mongodb.github.io/node-mongodb-native/2.0/tutorials/crud_operations/
+// aggregation: http://mongodb.github.io/node-mongodb-native/2.0/tutorials/aggregation/
+ 
+MongoClient.connect(url, function(err, db) {
+   assert.equal(null, err);
+   console.log("Connected correctly to server.");
+//var cursor = collection.find({});
+    // find top 20 countries by  size
+    db.collection('countries').find({},{"sort": [["area",-1]]}).limit(20).toArray(function(err, results){
+    console.log("Country One " +JSON.stringify(results[0])); 
+    console.log("Name of Country Four " +results[3].name+ " and size: " +results[3].area);
+ 
+      db.close();
+      console.log("Connection to database is closed.");
+    });
+ 
+}) //connect()
