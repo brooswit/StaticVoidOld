5f696ec5626bceaf9bdf7d471cf50bb35335c51d
brooswit <brooswit@OldBoi.local>
2018-11-30 00:43:33 -0800

    AUTOMATED DEPLOYMENT

--- elements/MemoryStore.js
+++ elements/MemoryStore.js
@@ -1,28 +1,6 @@
 const BaseStore = require('./BaseStore');
 
 module.exports = class MemoryStore extends BaseStore {
-  constructor (parent, options) {
-    super(parent, options);
-    this.parent.hook('save', ({path, value})=>{
-            
-    let pathElements = path.split('/');
-    let pointer = this.data;
-    for (let pathElementIndex = 0; pathElementIndex < pathElements.length - 1; pathElementIndex ++) {
-      pointer = pointer[pathElementIndex] === undefined ? {} : pointer[pathElementIndex];
-      pathElement = pathElements[pathElementIndex];
-    }
-    pointer[pathElements.length - 1] = value;
-  });
-  this.parent.hook('load', ({path, value})=>{
-  let pathElements = path.split('/');
-  let pointer = this.data;
-  for (let pathElementIndex = 0; pathElementIndex < pathElements.length - 1; pathElementIndex ++) {
-    pointer = pointer[pathElementIndex] === undefined ? {} : pointer[pathElementIndex];
-    pathElement = pathElements[pathElementIndex];
-  }
-    return pointer[pathElements.length - 1];
-  });
-}
     save(collection, key, value) {
         this.data[collection] = this.data[collection] || {};
         this.data[collection][key] = value;
