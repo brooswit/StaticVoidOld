626a79164beae1ce002b9c8bd732764a05f56a35
brooswit <brooswit@OldBoi.local>
2018-11-29 23:04:30 -0800

    AUTOMATED DEPLOYMENT

--- elements/BaseStore.js
+++ elements/BaseStore.js
@@ -4,7 +4,7 @@ module.exports = class MemoryStore extends Element {
   constructor (parent, state) {
     super(parent, state)
     this.rootView().hook('save', this._onSave);
-    this.rootView().hook('save', this._onSave);
+    this.rootView().hook('load', this._onLoad);
     this      let pathElements = path.split('/')
       let pointer = this.data
       for (let pathElementIndex = 0; pathElementIndex < pathElements.length - 1; pathElementIndex++) {
