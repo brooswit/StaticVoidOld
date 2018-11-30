e2f62d423732c86482732f5009fa049b3a2ba1cf
brooswit <brooswit@OldBoi.local>
2018-11-30 00:15:55 -0800

    AUTOMATED DEPLOYMENT

--- elements/BaseStore.js
+++ elements/BaseStore.js
@@ -15,7 +15,7 @@ function optsToFuncArgs (opts, func) {
   let unpack = true
   for (let argIndex in argNames) {
     let argName = argNames[argIndex]
-    let arg = opts[argIndex]
+    let arg = opts[argName]
     args[argName] = opts[argIndex]
     if (argIndex === 0 && (typeof arg !== 'object' || arg === null)) unpack = false
     if (argIndex > 0 && arg !== undefined) unpack = false
