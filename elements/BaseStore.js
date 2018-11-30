28a1e8b8d387f99dfb00d6753d3b5866cf3312f2
brooswit <brooswit@OldBoi.local>
2018-11-29 23:02:17 -0800

    AUTOMATED DEPLOYMENT

--- elements/BaseStore.js
+++ elements/BaseStore.js
@@ -1,25 +1,25 @@
-const Element = require('./Element');
+const Element = require('./Element')
 
 module.exports = class MemoryStore extends Element {
-    constructor(parent, state) {
-        super(parent, state);
-        this.parent.hook('save', ({path, value})=>{
-            let pathElements = path.split('/');
-            let pointer = this.data;
-            for (let pathElementIndex = 0; pathElementIndex < pathElements.length - 1; pathElementIndex ++) {
-                pointer = pointer[pathElementIndex] === undefined ? {} : pointer[pathElementIndex];
-                pathElement = pathElements[pathElementIndex];
-            }
-            pointer[pathElements.length - 1] = value;
-        });
-        this.parent.hook('load', ({path, value})=>{
-            let pathElements = path.split('/');
-            let pointer = this.data;
-            for (let pathElementIndex = 0; pathElementIndex < pathElements.length - 1; pathElementIndex ++) {
-                pointer = pointer[pathElementIndex] === undefined ? {} : pointer[pathElementIndex];
-                pathElement = pathElements[pathElementIndex];
-            }
-            return pointer[pathElements.length - 1];
-        });
-    }
+  constructor (parent, state) {
+    super(parent, state)
+    this.parent.hook('save', ({ path, value }) => {
+      let pathElements = path.split('/')
+      let pointer = this.data
+      for (let pathElementIndex = 0; pathElementIndex < pathElements.length - 1; pathElementIndex++) {
+        pointer = pointer[pathElementIndex] === undefined ? {} : pointer[pathElementIndex]
+        pathElement = pathElements[pathElementIndex]
+      }
+      pointer[pathElements.length - 1] = value
+    })
+    this.parent.hook('load', ({ path, value }) => {
+      let pathElements = path.split('/')
+      let pointer = this.data
+      for (let pathElementIndex = 0; pathElementIndex < pathElements.length - 1; pathElementIndex++) {
+        pointer = pointer[pathElementIndex] === undefined ? {} : pointer[pathElementIndex]
+        pathElement = pathElements[pathElementIndex]
+      }
+      return pointer[pathElements.length - 1]
+    })
+  }
 }
