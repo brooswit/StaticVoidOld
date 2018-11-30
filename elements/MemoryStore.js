98cd213cbe521800aa504d661e97b976ac5afe5f
brooswit <brooswit@OldBoi.local>
2018-11-30 00:46:51 -0800

    AUTOMATED DEPLOYMENT

--- elements/MemoryStore.js
+++ elements/MemoryStore.js
@@ -1,18 +1,20 @@
 const BaseStore = require('./BaseStore')
 
 module.exports = class MemoryStore extends BaseStore {
-  save (collection, key, value) {
+  async save (collection, key, value) {
     this.data[collection] = this.data[collection] || {}
     this.data[collection][key] = value
   }
-  load (collection, key, optionalDefault) {
+
+  async load (collection, key, optionalDefault) {
     let value = optionalDefault || null
     if (this.data[collection]) {
     }
     this.data[collection] = this.data[collection] || {}
     this.data[collection][key] = value
   }
-  has (collection, key) {
+  
+  async has (collection, key) {
     if (!this.data[collection]) return false
     if (!this.data[collection][key]) return false
     return true
