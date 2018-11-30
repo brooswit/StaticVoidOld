bcd0717299fe86e1ebdbd85d7294097bd94c0332
brooswit <brooswit@OldBoi.local>
2018-11-30 00:33:02 -0800

    AUTOMATED DEPLOYMENT

--- elements/Element.js
+++ elements/Element.js
@@ -197,7 +197,7 @@ class Element {
     this._rootView = new ElementView(this.root())
 
     this.parentView().hook('destroyed', this.destroy)
-    this.parentView().hook(`find_${this.name.toLowerCase()}_children`, this._getThis)
+    this.parentView().hook(`${this.name.toLowerCase()}_children_views`, this.view)
     this.parentView().hook('find_children', this._getThis)
 
     this.rootView().hook(`find_all_${this.name.toLowerCase()}`, this._getThis)
