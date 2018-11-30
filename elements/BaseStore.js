14570c9877d812a0d5fbdc0acf54b359ef149a34
brooswit <brooswit@OldBoi.local>
2018-11-29 23:52:05 -0800

    AUTOMATED DEPLOYMENT

--- elements/BaseStore.js
+++ elements/BaseStore.js
@@ -1,25 +1,5 @@
 const Element = require('./Element')
-function getArgumentNames (func) {
-  let argumentNames = null
-  try {
-    argumentNames = func.toString().split('(')[1].split(')')[0].replace(/\s/g, '').split(',')
-  } catch (e) {}
-  return argumentNames
-}
 
-function extractArguments (args) {
-  let options = {}
-  let argNames = getArgumentNames(args.callee)
-  let unpack = true
-  for (let argIndex in argNames) {
-    let argName = argNames[argIndex]
-    let arg = args[argIndex]
-    options[argName] = args[argIndex]
-    if (argIndex === 0 && (typeof arg !== 'object' || arg === null)) unpack = false
-    if (argIndex > 0 && arg !== undefined) unpack = false
-  }
-  if (unpack) options = args[0]
-}
 
 module.exports = class BaseStore extends Element {
   constructor (parent, state) {
--- utility.js
+++ utility.js
@@ -1,19 +1,41 @@
-function getBaseMethod() {
-    let stackTrace = new StackTrace()
-    let methodBase = stackTrace.GetFrame(2).GetMethod()
+function getBaseMethod () {
+  let stackTrace = new StackTrace()
+  let methodBase = stackTrace.GetFrame(2).GetMethod()
 }
 
-function asyncGate(asyncFunc) {
-    if (methodBase._instance) return methodBase._instance
-    getBaseMethod()._instance = asyncFunc()
+function asyncGate (asyncFunc) {
+  if (methodBase._instance) return methodBase._instance
+  getBaseMethod()._instance = asyncFunc()
 
-    (async()=>{
-        await methodInstance
-        methodBase._instance = null
-    })
-    return methodBase._instance
+  (async () => {
+    await methodInstance
+    methodBase._instance = null
+  })
+  return methodBase._instance
 }
 
-async function asyncWait() {
-    if (methodBase._instance) await methodBase._instance
+async function asyncWait () {
+  if (methodBase._instance) await methodBase._instance
 }
+
+function getArgumentNames (func) {
+    let argumentNames = null
+    try {
+      argumentNames = func.toString().split('(')[1].split(')')[0].replace(/\s/g, '').split(',')
+    } catch (e) {}
+    return argumentNames
+  }
+  
+  function extractArguments (args) {
+    let options = {}
+    let argNames = getArgumentNames(args.callee)
+    let unpack = true
+    for (let argIndex in argNames) {
+      let argName = argNames[argIndex]
+      let arg = args[argIndex]
+      options[argName] = args[argIndex]
+      if (argIndex === 0 && (typeof arg !== 'object' || arg === null)) unpack = false
+      if (argIndex > 0 && arg !== undefined) unpack = false
+    }
+    if (unpack) options = args[0]
+  }
