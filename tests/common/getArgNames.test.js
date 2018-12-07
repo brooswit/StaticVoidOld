5bb0b5a8c5b901e2355b6a2b7ce0d42e127f3e0f
brooswit <brooswit@OldBoi.local>
2018-12-06 21:09:02 -0800

    AUTOMATED DEVELOPMENT DEPLOYMENT

--- tests/common/getArgNames.test.js
+++ tests/common/getArgNames.test.js
@@ -7,7 +7,14 @@ test('outputs array argument names for a function', () => {
   expect(funcArgs).toEqual(expectedArgs)
 })
 
-test('outputs array argument names for an', () => {
+test('outputs array argument names for a callback', () => {
+  let func = (a, b, c) => {}
+  let funcArgs = getArgNames(func)
+  let expectedArgs = ['a', 'b', 'c']
+  expect(funcArgs).toEqual(expectedArgs)
+})
+
+test('outputs array argument names for an async callback', () => {
   let func = (a, b, c) => {}
   let funcArgs = getArgNames(func)
   let expectedArgs = ['a', 'b', 'c']
